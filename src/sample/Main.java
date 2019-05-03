8f5908c6bc15bb274d725459ead113099368a8a4
effer <efimenkoav86@gmail.com>
2019-05-03 19:30:39 +0400

    добавлен count в main класс, удалены строки по умолчанию

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,27 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
 public class Main extends Application {
-
+    private static int count;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Group root=new Group();
+        primaryStage.setTitle("cockroach runners");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        try{count=Integer.parseInt(args[0]);}
+        catch(Exception ex){
+            System.out.println("Nado vvesti chislo");
+        }
+        if(count==0)System.exit(0);
         launch(args);
     }
 }
