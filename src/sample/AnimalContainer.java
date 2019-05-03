d8d7ced7ab5fd00a2e33ef31ee40daac4616ef5b
effer <efimenkoav86@gmail.com>
2019-05-03 19:45:51 +0400

    Добавлены интерфейсы AnimalDrawer и AnimalContainer

---
+++ src/sample/AnimalContainer.java
@@ -0,0 +1,8 @@
+package sample;
+
+public interface AnimalContainer extends Runnable{
+    public void run();
+    public void start();
+    public void reset();
+    public void boost();
+}
---
+++ src/sample/AnimalDrawer.java
@@ -0,0 +1,5 @@
+package sample;
+
+public interface AnimalDrawer {
+    public void Draw();
+}
